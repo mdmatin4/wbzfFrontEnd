738452955f327a300da17fa78467de0076900966
Matin <mdmatin4@gmail.com>
2023-05-28 00:48:42 +0530

    sizeissue

--- src/app/pages/superset-chart/superset-chart.component.ts
+++ src/app/pages/superset-chart/superset-chart.component.ts
@@ -1,5 +1,5 @@
 import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild, ViewEncapsulation } from '@angular/core';
 import { embedDashboard } from "@superset-ui/embedded-sdk";
 import { Observable } from 'rxjs';
 import { SupersetAuthServicesService } from 'src/app/services/superset-auth-services.service';
@@ -9,31 +9,32 @@ import { environment } from 'src/environments/environment';
 @Component({
   selector: 'app-superset-chart',
   templateUrl: './superset-chart.component.html',
-  styleUrls: ['./superset-chart.component.scss']
+  styleUrls: ['./superset-chart.component.scss'],
+  encapsulation : ViewEncapsulation.None
 })
-export class SupersetChartComponent {
+export class SupersetChartComponent  {
+  @ViewChild('myDiv', { static: false }) myDiv!: ElementRef;
   constructor(private http: HttpClient,private supersetAuthService : SupersetAuthServicesService) {
     this.supersetAuthService.login();
     this._loginChart.access_token=localStorage.getItem("access_token")!;
     this._loginChart.refresh_token=localStorage.getItem("refrest_token")!;
-    this.guest_token=this.fetchGuestTokenFromBackend();
 
   }
   baseURL =(environment as any).baserUrl_superset;
   invalidLogin : boolean= true;
-   guest_token: Promise<string>;
    _loginChart: loginChart={access_token:"",refresh_token:""};
+
+
+   
 
 
   ngOnInit(){
     
- 
-
 embedDashboard({
   id: "b921ea83-5668-401c-bbc0-5a2e5adca614", // given by the Superset embedding UI
   supersetDomain: "http://4.155.236.214:8088",
-  mountPoint : document.getElementById("my-superset-container")!, // any html element that can contain an iframe
-  fetchGuestToken: () => this.guest_token ,
+  mountPoint : document.getElementById("myDiv")!, // any html element that can contain an iframe
+  fetchGuestToken: () => this.fetchGuestTokenFromBackend(),
   dashboardUiConfig: { // dashboard UI config: hideTitle, hideTab, hideChartControls, filters.visible, filters.expanded (optional)
       hideTitle: true,
       hideTab: false,
@@ -46,7 +47,26 @@ embedDashboard({
 });
 
 
-
+}
+ngAfterViewInit() {
+  
+  setTimeout(() => {
+    if(this.myDiv){
+      const iframe=document.getElementById('myDiv')?.firstChild as HTMLIFrameElement;
+      console.log(iframe);
+      if (iframe) {
+          this.resizeIframeToFitContent(iframe);
+      }
+      
+    }
+  }, 100);
+  
+  
+}
+resizeIframeToFitContent(iframe: HTMLIFrameElement) {
+  
+  iframe.style.height = iframe.contentWindow?.document.body.scrollHeight + 'px';
+  console.log(iframe.style.height);
 }
 
 
@@ -73,13 +93,13 @@ fetchGuestTokenFromBackend(): Promise<string> {
     }
   }
   
-  return this.http.post<JSON>(`${this.baseURL}/security/guest-token/`,body,requestOptions).toPromise()
+  return this.http.post<JSON>(`${this.baseURL}/security/guest_token/`,body,requestOptions).toPromise()
     .then((response: any)  => {
       
-      this.guest_token=response.token;
+      var guest_token=response.token;
       // this.checkAndRefreshToken();
-      console.log(this.guest_token);
-      return this.guest_token;
+      console.log(guest_token);
+      return guest_token;
     })
     .catch((err : HttpErrorResponse ) => {
       console.log(err);
